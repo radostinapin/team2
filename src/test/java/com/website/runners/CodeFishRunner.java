1d756e5b4acdffd5586737833aaf7666c208767d
Aida <ayida.ivey@gmail.com>
2023-05-23 14:32:31 -1000

    Added

--- src/test/java/com/website/runners/CodeFishRunner.java
+++ src/test/java/com/website/runners/CodeFishRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features/website",
         glue = "com/website/stepdefinitions",
         dryRun = false,
-        tags = "@positivelogin",
+        tags = "@cartfunctionality",
         plugin = {"pretty","html:target/uiReport.html","rerun:target/uiFailedTests.txt",
                   "json:target/cucumber-reports/cucumber.json"}
 
